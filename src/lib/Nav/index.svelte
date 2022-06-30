a89979774626bea8bb82f64e97b1d8e495a85222
Jay Onyschuk <jayonyschuk@gmail.com>
2022-06-29 22:50:06 -0600

    added drop down menu for testing out the env

--- src/lib/Nav/index.svelte
+++ src/lib/Nav/index.svelte
@@ -1,20 +1,26 @@
 <script>
-	import { tabs } from '$lib/utils/tabs';
-	import NavSmall from './NavSmall.svelte';
-	import NavLarge from './NavLarge.svelte';
-	
-	import IconButton from '@smui/icon-button';
-	import { Icon } from '@smui/common';
+	import { tabs } from "$lib/utils/tabs";
+	import NavSmall from "./NavSmall.svelte";
+	import NavLarge from "./NavLarge.svelte";
+
+	import IconButton from "@smui/icon-button";
+	import { Icon } from "@smui/common";
+	import Select, { Option } from "@smui/select";
 
 	export let activeTab;
 
-	$: active = tabs.find(tab => tab.dest == activeTab || (tab.nest && tab.children.find(subTab => subTab.dest == activeTab)));
+	$: active = tabs.find(
+		(tab) =>
+			tab.dest == activeTab ||
+			(tab.nest &&
+				tab.children.find((subTab) => subTab.dest == activeTab))
+	);
 
 	// toggle dark mode
 	let lightTheme =
 		typeof window === "undefined" ||
 		window.matchMedia("(prefers-color-scheme: light)").matches;
-	
+
 	function switchTheme() {
 		lightTheme = !lightTheme;
 		let themeLink = document.head.querySelector("#theme");
@@ -25,19 +31,60 @@
 		}
 		themeLink.href = `/smui${lightTheme ? "" : "-dark"}.css`;
 		document.head
-		.querySelector('link[href="/smui-dark.css"]')
-		.insertAdjacentElement("afterend", themeLink);
+			.querySelector('link[href="/smui-dark.css"]')
+			.insertAdjacentElement("afterend", themeLink);
 	}
+
+	const league_Ids = ["814387382903218176", "moreids"];
+	console.log("leagueIds", league_Ids);
 </script>
 
 <svelte:head>
-	<title>{!activeTab[1] ? 'Home' : activeTab[1].toUpperCase() + activeTab.slice(2)} | League Page</title>
+	<title
+		>{!activeTab[1]
+			? "Home"
+			: activeTab[1].toUpperCase() + activeTab.slice(2)} | League Page</title
+	>
 </svelte:head>
+
+<nav>
+	<a href="/"><img id="logo" alt="league logo" src="./badge.png" /></a>
+
+	<div class="container">
+		<Select
+			class="shaped-outlined"
+			variant="outlined"
+			label="Your Sleeper League ID"
+			size="6"
+		>
+			{#each league_Ids as id}
+				<Option value={id}>{id}</Option>
+			{/each}
+		</Select>
+		<IconButton
+			toggle
+			pressed={lightTheme}
+			on:MDCIconButtonToggle:change={switchTheme}
+			class="lightDark"
+		>
+			<Icon class="material-icons" on>dark_mode</Icon>
+			<Icon class="material-icons">light_mode</Icon>
+		</IconButton>
+	</div>
+
+	<div class="large">
+		<NavLarge {tabs} bind:active />
+	</div>
+
+	<div class="small">
+		<NavSmall {tabs} bind:active={activeTab} />
+	</div>
+</nav>
 
 <style>
 	a {
 		display: table;
-    	margin: 0 auto;
+		margin: 0 auto;
 	}
 	nav {
 		background-color: var(--fff);
@@ -54,9 +101,9 @@
 		padding: 10px;
 	}
 
-    .large {
+	.large {
 		display: block;
-    }
+	}
 
 	.small {
 		display: none;
@@ -69,10 +116,11 @@
 	}
 
 	:global(.lightDark) {
-		color: var(--g555)
+		color: var(--g555);
 	}
 
-	@media (max-width: 950px) { /* width of the large navBar */
+	@media (max-width: 950px) {
+		/* width of the large navBar */
 		.large {
 			display: none;
 		}
@@ -82,28 +130,3 @@
 		}
 	}
 </style>
-
-<nav>
-	<a href="/"><img id="logo" alt="league logo" src="./badge.png" /></a>
-
-	<div class="container">
-		<IconButton
-			toggle
-			pressed={lightTheme}
-			on:MDCIconButtonToggle:change={switchTheme}
-			class="lightDark"
-		>
-			<Icon class="material-icons" on>dark_mode</Icon>
-			<Icon class="material-icons">light_mode</Icon>
-		</IconButton>
-	</div>
-
-	<div class="large">
-		<NavLarge {tabs} bind:active={active} />
-	</div>
-
-	<div class="small">
-		<NavSmall {tabs} bind:active={activeTab} />
-	</div>
-
-</nav>
